831f668863ecb5b54b47bc30c1c4db590c0f1c1d
Sarang <bhongsarang007@gmail.com>
2024-08-28 13:55:40 +0530

    First Comment

---
+++ unlimited Color/unlimited.js
@@ -0,0 +1,85 @@
+let start =document.querySelector('#start');
+
+
+console.log(start)
+
+let stop =document.querySelector('#stop')
+
+console.log(stop)
+
+
+let body =document.querySelector('body')
+
+console.log(body)
+
+let arra =[]
+let color=[
+
+
+'#F0F8FF',
+
+'#FAEBD7',
+
+'#00FFFF',
+
+'#7FFFD4',
+
+'#F0FFFF',
+
+
+'#FFE4C4',
+
+'#000000',
+
+'#FFEBCD',
+
+'#0000FF',
+
+'#8A2BE2',
+
+'#A52A2A',
+
+'#DEB887',
+
+'#5F9EA0',
+
+'#7FFF00',
+
+'#D2691E',
+
+'#FF7F50',
+
+'#6495ED',
+
+'#FFF8DC',
+
+]
+
+start.addEventListener('click',function(e){
+  
+    let i = 0; //
+    setIntervId=setInterval(function(){
+       
+      let colors=  body.style.backgroundColor = color[i % color.length];
+
+      console.log("last color:-"+colors)
+          i++
+  
+        },1000)
+
+
+});
+stop.addEventListener('click',function(){
+
+clearInterval(setIntervId);
+    body.style.background=setIntervId.colors;
+
+})
+// setInterval(function(){
+
+// for(let i=0;i>color.length;i++){
+//     body.style.backgroundColor= 'color';
+
+// }
+
+// },1000)
